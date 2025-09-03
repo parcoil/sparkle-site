c082d06cb3e233e6835c8cd12a1d850b8e09f7ee
Thedogecraft <dantaealt@gmail.com>
2025-09-03 01:02:06 -0700

    fix: posthog

--- src/lib/server/posthog.ts
+++ src/lib/server/posthog.ts
@@ -1,13 +1,12 @@
 import posthog, { PostHog } from 'posthog-node';
-import { PUBLIC_POSTHOG_KEY, PUBLIC_POSTHOG_HOST } from '$env/static/public';
 
 let _client: PostHog | null = null;
 
 export function getPostHogClient() {
-  if (!_client) {
-    _client = new posthog.PostHog(PUBLIC_POSTHOG_KEY, {
-      host: PUBLIC_POSTHOG_HOST,
-    });
-  }
-  return _client;
+	if (!_client) {
+		_client = new posthog.PostHog('phc_pXPMfdpgrBN9eT7hFmaRZIzLJuC87f3KeCwdlMi5p2W', {
+			host: 'https://us.i.posthog.com'
+		});
+	}
+	return _client;
 }
