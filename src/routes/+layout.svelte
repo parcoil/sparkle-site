80a3f2c7dc8ed8b3a95a31d2f1a1f79d07943f7d
Thedogecraft <dantaealt@gmail.com>
2025-09-03 00:17:18 -0700

    fix: update seo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,31 +23,6 @@
 		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1565760898646999"
 		crossorigin="anonymous"
 	></script>
-	<meta
-		property="og:description"
-		content="Free, open-source tool to optimize Windows, remove bloatware, and boost your PC's performance. Download now for a faster, cleaner Windows experience."
-	/>
-	<meta property="og:image" content="/sparklelogo.png" />
-	<meta property="og:image:width" content="1200" />
-	<meta property="og:image:height" content="630" />
-	<meta property="og:image:alt" content="Sparkle - Ultimate Windows Optimizer" />
-
-	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:title" content="Sparkle | Ultimate Windows Optimizer" />
-	<meta
-		name="twitter:description"
-		content="Free, open-source tool to optimize Windows, remove bloatware, and boost your PC's performance. Download now!"
-	/>
-	<meta name="twitter:image" content="/sparklelogo.png" />
-
-	<meta
-		name="description"
-		content="Free, open-source tool to optimize Windows, remove bloatware, and boost your PC's performance. Download Sparkle for a faster, cleaner Windows experience."
-	/>
-	<meta
-		name="keywords"
-		content="Windows optimizer, PC optimization, remove bloatware, speed up Windows, free PC cleaner, Windows tweaks, system optimization, gaming performance, Windows debloater, free system utility"
-	/>
 </svelte:head>
 
 <ModeWatcher />
